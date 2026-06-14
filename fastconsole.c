a34b2a36311229b9af81b7d8cc6565fff80f99e2
Stephen Sviatko <ssviatko@gmail.com>
2026-06-14 14:50:08 -0700

    clean up fastconsole.c

--- fastconsole.c
+++ fastconsole.c
@@ -3,43 +3,82 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <stdint.h>
+#include <getopt.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-int g_width = 960;
-int g_height = 544;
-int g_scale = 2;
+#define DEFAULTX 480
+#define DEFAULTY 272
+
+struct option g_options[] = {
+    { "scale", required_argument, NULL, 's' },
+    { "exec", required_argument, NULL, 'e' },
+    { NULL, 0, NULL, 0 }
+};
+
+unsigned int g_width = DEFAULTX;
+unsigned int g_height = DEFAULTY;
+unsigned int g_scale = 1;
+
+const uint32_t g_standard_colors[16] = {
+    0xff000000, // Black
+    0xffdd0033, // Deep Red
+    0xff000099, // Dark Blue
+    0xffdd22dd, // Purple
+    0xff007722, // Dark Green
+    0xff555555, // Dark Gray
+    0xff2222ff, // Medium Blue
+    0xff66aaff, // Light Blue
+    0xff885500, // Brown
+    0xffff6600, // Orange
+    0xffaaaaaa, // Light Gray
+    0xffff9988, // Pink
+    0xff11dd00, // Light Green
+    0xffffff00, // Yellow
+    0xff44ff99, // Aquamarine
+    0xffffffff  // White
+};
 
 int main(int argc, char **argv)
 {
+    int i;
     srand(time(NULL));
+    int opt;
+    while ((opt = getopt_long(argc, argv, "s:e:", g_options, NULL)) != -1) {
+        switch (opt) {
+            case 's':
+                g_scale = atoi(optarg);
+                break;
+            case 'e':
+//                load_server(optarg);
+                break;
+        }
+    }
+
+    // sanity check the scale
+    if ((g_scale < 1) || (g_scale > 8)) {
+        fprintf(stderr, "fconsole: scale value must be between 1-8.\n");
+        exit(-1);
+    }
+
+    // set height/g_width
+    g_width = DEFAULTX * g_scale;
+    g_height = DEFAULTY * g_scale;
+    printf("fconsole: selecting %dx%d window (scale %d)\n", g_width, g_height, g_scale);
 
     Display *dpy;
-    XVisualInfo vinfo;
-    int depth;
-    XVisualInfo *visual_list;
-    XVisualInfo visual_template;
-    int nxvisuals;
-    int i;
     XSetWindowAttributes attrs;
     Window parent;
-    Visual *visual;
-
     Window win;
-    int *framebuf;
-    XImage *ximage;
-    XEvent event;
 
     dpy = XOpenDisplay(NULL);
 
-    nxvisuals = 0;
-    visual_template.screen = DefaultScreen(dpy);
-    visual_list = XGetVisualInfo (dpy, VisualScreenMask, &visual_template, &nxvisuals);
-
+    XVisualInfo vinfo;
     if (!XMatchVisualInfo(dpy, XDefaultScreen(dpy), 24, TrueColor, &vinfo))
     {
-        fprintf(stderr, "no such visual\n");
+        fprintf(stderr, "fconsole: no such visual\n");
         return 1;
     }
 
@@ -47,15 +86,19 @@ int main(int argc, char **argv)
 
     XSync(dpy, True);
 
-    printf("creating RGBA child\n");
+    printf("fconsole: creating frame buffer...\n");
 
+    Visual *visual;
     visual = vinfo.visual;
+
+    int depth;
     depth = vinfo.depth;
 
     attrs.colormap = XCreateColormap(dpy, XDefaultRootWindow(dpy), visual, AllocNone);
     attrs.background_pixel = 0;
     attrs.border_pixel = 0;
 
+    int *framebuf;
     framebuf = (int *) malloc((g_width * g_height) * 4);
 
     for (i = 0; i < (g_width * g_height); i++)
@@ -75,7 +118,7 @@ int main(int argc, char **argv)
     sizehints.max_height = g_height;
     XSetWMNormalHints(dpy, win, &sizehints);
 
-    ximage = XCreateImage(dpy, vinfo.visual, depth, ZPixmap, 0, (char *)framebuf, g_width, g_height, 8, g_width * 4);
+    XImage *ximage = XCreateImage(dpy, vinfo.visual, depth, ZPixmap, 0, (char *)framebuf, g_width, g_height, 8, g_width * 4);
 
     if (ximage == 0)
     {
@@ -102,6 +145,7 @@ int main(int argc, char **argv)
 
     XMapWindow(dpy, win);
 
+    XEvent event;
     int runFlag = 1;
     while (runFlag == 1) {
 
@@ -123,12 +167,12 @@ int main(int argc, char **argv)
             XNextEvent(dpy, &event);
             switch(event.type) {
             case Expose:
-                printf("I have been exposed!\n");
+//                printf("I have been exposed!\n");
                 XPutImage(dpy, win, NormalGC, ximage, 0, 0, 0, 0, g_width, g_height);
                 break;
             case ClientMessage:
                 char *str = XGetAtomName(dpy, event.xclient.message_type);
-                printf("ClientMessage: %s\n",str);
+//                printf("ClientMessage: %s\n",str);
                 if (!strcmp(str,"WM_PROTOCOLS")) {
                     runFlag = 0;
                     XFree(str);
@@ -138,7 +182,7 @@ int main(int argc, char **argv)
         }
     }
 
-    printf("Exiting...\n");
+    printf("fconsole: exiting...\n");
 
     return 0;
 }
